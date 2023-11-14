7fe188dcbd763cdce3d327ee3b3505323c1b8c36
msscsh <itsbruno42@gmail.com>
2023-11-14 13:07:21 -0300

    adding project source for low level playground

---
+++ playgrounds/lowlevel/do.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Verify prerequisites
+
+mkdir -p ~/dev/playgrouds/
+cd ~/dev/playgrouds/
+git clone https://github.com/msscsh/playground-lowlevel.git
---
+++ playgrounds/lowlevel/undo.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+
+#undo init.sh script here
