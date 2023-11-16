8b6bb9795598d4b29f908d8cac8c60cea963a194
msscsh <itsbruno42@gmail.com>
2023-11-16 17:11:36 -0300

    final adjustments for rust environment

---
+++ enviroments/rust/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Verify prerequisites
+echo $PWD
+sudo apt update
+sudo apt install rustc
+
+rustc --version
+
+rustc ./enviroments/rust/test.rs -o ./enviroments/rust/test
+./enviroments/rust/test
+rm -f ./enviroments/rust/test
--- enviroments/rust/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Verify prerequisites
-echo $PWD
-sudo apt update
-sudo apt install rustc
-rustc ./enviroments/rust/rust-test.rs -o ./enviroments/rust/rust-test
-./enviroments/rust/rust-test
-rm -f ./enviroments/rust/rust-test
