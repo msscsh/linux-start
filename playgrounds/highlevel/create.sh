215d59132ae9bcb51378e7b2f2d466424dd4a067
msscsh <itsbruno42@gmail.com>
2023-11-16 17:20:58 -0300

    adding high level playground into project

---
+++ playgrounds/highlevel/create.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Verify prerequisites
+mkdir -p ~/dev/playgrounds/
+git clone https://github.com/msscsh/playground-highlevel.git ~/dev/playgrounds/playground-highlevel/
---
+++ playgrounds/highlevel/destroy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo rm -rf ~/dev/playgrounds/playground-highlevel
