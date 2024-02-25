40c4cc8794ad53c650bbb043235e33ca12a78b8f
msscsh <itsbruno42@gmail.com>
2024-02-25 20:26:43 -0300

    adding bulldozer enviroment

---
+++ enviroments/buildozer/install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y python3-pip build-essential git python3 python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
+
+#android
+sudo apt-get install autoconf automake libtool
+
+sudo pip3 install --upgrade cython pillow
+sudo pip3 install buildozer
